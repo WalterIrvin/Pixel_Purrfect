fb91a800bedef94f48fcf0eb38682049b2be8f81
Sean Fricke <seanfricke03@gmail.com>
2024-06-23 22:40:27 -0400

    Add placeholder movement script (WIP)

---
+++ scenes/entities/Player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var H_Axis = Input.get_axis("ui_left", "ui_right")
+	var V_Axis = Input.get_axis("ui_down", "ui_up")
+	if H_Axis or V_Axis:
+		global_translate(Vector2(global_position.x + H_Axis, global_position.y + V_Axis))
