9ac165ec84e320bc8d3e23754d060d578d15e060
Sean Fricke <seanfricke03@gmail.com>
2024-06-23 22:41:30 -0400

    Add placeholder movement script (WIP 2)

---
+++ scenes/scripts/Movement_PLACEHOLDER.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+func _process(delta):
+	const speed = 0.01
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var H_Axis = Input.get_axis("ui_left", "ui_right")
+	var V_Axis = Input.get_axis("ui_down", "ui_up")
+	var offset: Vector2 = Vector2(H_Axis * delta * speed, V_Axis * delta * speed)
+	print(offset)
+	print(H_Axis, V_Axis)
+	if H_Axis or V_Axis:
+		global_translate(global_position + offset)
